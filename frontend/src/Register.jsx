a2191aa4003360cfd13a9b1ef0987ec89fb72aca
anuj1101 <kranuj952@gmail.com>
2026-02-27 09:29:08 +0530

    start building register page

---
+++ frontend/src/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Register = () => {
+    const [data,setData]=useState({});
+  return (
+    <div>
+      <form onSubmit={handleSubmit}></form>
+    </div>
+  )
+}
+
+export default Register
